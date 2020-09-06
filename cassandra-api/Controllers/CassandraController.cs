d4d2ea6c319cd2b2aaefb93c96ebfbfb36d9d448
resul guldibi <resulguldibi@gmail.com>
2020-09-06 17:04:46 +0300

    000 - resul adds .net core & cassandra sample operations

---
+++ cassandra-api/Controllers/CassandraController.cs
@@ -0,0 +1,39 @@
+﻿using client.cassandra.core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace cassandra_api.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class CassandraController : ControllerBase
+    {
+        private readonly ILogger<CassandraController> _logger;
+        private readonly ICassandraClientProvider _cassandraClientProvider;
+        public CassandraController(ILogger<CassandraController> logger, ICassandraClientProvider cassandraClientProvider)
+        {
+            _logger = logger;
+            _cassandraClientProvider = cassandraClientProvider;
+        }
+
+
+        [HttpGet]
+        [ActionName("samples")]
+        public async Task<IEnumerable<SampleEntity>> GetDataFromCassandra()
+        {
+            using (ICassandraClient client = this._cassandraClientProvider.GetCassandraClient("my_cluster", "my_keyspace"))
+            {
+                return client.Fetch<SampleEntity>("SELECT * FROM my_table");
+            }
+        }
+    }
+
+    public class SampleEntity
+    {
+        public string Id { get; set; }
+        public int Count { get; set; }
+        public string Word { get; set; }
+    }
+}
---
+++ cassandra-api/Startup.cs
@@ -0,0 +1,51 @@
+using client.cassandra.core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace cassandra_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddSingleton<ICassandraConnectionInfoProvider, CassandraConnectionInfoProvider>();
+            services.AddSingleton<ICassandraClusterProvider, CassandraClusterProvider>();
+            services.AddSingleton<ICassandraSessionProvider, CassandraSessionProvider>();
+            services.AddSingleton<ICassandraClientProvider, CassandraClientProvider>();            
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
